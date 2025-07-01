0fb11b70d495deca9ea415963902da246518586e
OceanWhisperer <arnavmahajan630@gmail.com>
2025-07-01 10:21:54 +0530

    feat/restructured-mongodb-connected

--- main.go
+++ main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	db "github.com/arnavmahajan630/go-hrms-api/DB"
+	"github.com/gofiber/fiber/v2"
+)
+
+
 
 func main() {
-	fmt.Printf("check clone pull")
+	err := db.Connect()
+	if err != nil {
+		log.Fatal(err)
+	}
+	 app := fiber.New()
+	// app.Get("/employee")
+	// app.Post("/employee")
+	// app.Put("/employee/:id")
+	// app.Delete("/employee/:id")
+    
+
+	fmt.Println("Server Started on Port 8080")
+	err = app.Listen(":8080")
+	if err != nil {
+		fmt.Println(err)
+	}
+
 }
